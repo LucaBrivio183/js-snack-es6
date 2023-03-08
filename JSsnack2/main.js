39025220521cf949e76266fcb667cbdb61ca219f
LucaBrivio183 <l.brivio4@gmail.com>
2023-03-08 16:48:59 +0100

    feat: created students  obj array

--- JSsnack2/main.js
+++ JSsnack2/main.js
@@ -1,3 +1,40 @@
 'use strict'
 
-alert('hello')
+//array of objects   
+const students = [
+    {
+        Id: 213,
+        Name: 'MARCO DELLA ROVERE',
+        Grades: 78, 
+    },
+    {
+        Id: 110,
+        Name: 'PAOLA CORTELLESSA',
+        Grades: 96, 
+    },
+    {
+        Id: 250,
+        Name: 'ANDREA MANTEGNA',
+        Grades: 48, 
+    },
+    {
+        Id: 145,
+        Name: 'GAIA BORROMINI',
+        Grades: 74, 
+    },
+    {
+        Id: 196,
+        Name: 'LUIGI GRIMALDELLO',
+        Grades: 68, 
+    },
+    {
+        Id: 102,
+        Name: 'PIERO DELLA FRANCESCA',
+        Grades: 50, 
+    },
+    {
+        Id: 12,
+        Name: 'FRANCESCA DA POLENTA',
+        Grades: 84, 
+    },
+]
