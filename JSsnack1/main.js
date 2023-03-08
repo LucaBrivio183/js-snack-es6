fa32a8f620f2e2f1aa30d840d33a4ad2266ac122
LucaBrivio183 <l.brivio4@gmail.com>
2023-03-08 16:35:24 +0100

    feat: excercise done

--- JSsnack1/main.js
+++ JSsnack1/main.js
@@ -1,3 +1,25 @@
 'use strict'
 
-alert('hello');
+//guestsName variable
+
+const guestsNames = ['Brad Pitt',
+    'Johnny Depp', 
+    'Lady Gaga', 
+    'Cristiano Ronaldo',
+    'Georgina Rodriguez',
+    'Chiara Ferragni',
+    'Fedez',
+    'George Clooney',
+    'Amal Clooney',
+    'Maneskin']
+
+
+const guests = guestsNames.map ((item, index) => {
+  return { 
+    table: 'Tavolo Vip',
+    name: item,
+    seat: index + 1,
+  };
+});
+
+console.log(guests);
